58ea14f3705a51ee7c93d167f12bb1e4aa0526cf
Kevin <378502388@qq.com>
2018-01-10 14:16:27 +0800

    加缓解和屏蔽门

---
+++ ATS/PSDoor.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows.Media;
+using 线路绘图工具;
+
+namespace CBI_LCP
+{
+    public class PSDoor : 线路绘图工具.PSDoor, ITrackSide
+    {
+        static Pen DisconnectPen = new Pen(Brushes.Yellow, 3);
+
+        public static int StartByte { get; set; }
+
+        public bool IsStatusChanged { get; set; }
+
+        public bool IsFailed { get; }
+
+        int startByte_;
+        bool isOccupied_ = true;
+        bool IsOccupied
+        {
+            get { return isOccupied_; }
+            set
+            {
+                if (value != isOccupied_)
+                {
+                    isOccupied_ = value;
+                    IsStatusChanged = true;
+                }
+            }
+        }
+        bool isDisconnected_ = false;
+        bool IsDisconnected
+        {
+            get { return isDisconnected_; }
+            set
+            {
+                if (value != isDisconnected_)
+                {
+                    isDisconnected_ = value;
+                    IsStatusChanged = true;
+                }
+            }
+        }
+
+        public void ResetDefaultStatus()
+        {
+            IsOccupied = true;
+        }
+
+        public void SetCommandMenu()
+        {
+        }
+
+        public void SetStartByte(int ID)
+        {
+            startByte_ = StartByte + ID;
+        }
+
+        public void SetLocalStartByte(CiStartId startID)
+        {
+            SetStartByte(ID - startID.PSDoorStart);
+        }
+
+        public void UpdateStatus(byte[] recvBuf, int nRecv)
+        {
+            const byte TRUE_VALUE = 0X05;
+            IsDisconnected = (recvBuf[startByte_] >> 4) == TRUE_VALUE;
+            IsOccupied = (recvBuf[startByte_] & 0x0f) == TRUE_VALUE;
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            Pen pen = IsDisconnected ? DisconnectPen : DefaultPen_;
+            
+            (graphics_[0] as Line).OnRender(dc, pen);
+            (graphics_[2] as Line).OnRender(dc, pen);
+
+            if (!IsOccupied && !IsDisconnected)
+            {
+                (graphics_[1] as Line).OnRender(dc, DefaultPen_);
+            }
+        }
+    }
+}
---
+++ ATS/RelayButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Media;
+using 线路绘图工具;
+
+namespace CBI_LCP
+{
+    public class RelayButton : 线路绘图工具.RelayButton, ITrackSide
+    {
+        public bool IsStatusChanged { get; set; }
+
+        bool ITrackSide.IsFailed { get; }
+
+        int startByte_;
+        bool isOccupied_ = true;
+        bool IsOccupied
+        {
+            get { return isOccupied_; }
+            set
+            {
+                if (value != isOccupied_)
+                {
+                    isOccupied_ = value;
+                    IsStatusChanged = true;
+                }
+            }
+        }
+
+        public void ResetDefaultStatus()
+        {
+            isOccupied_ = true;
+        }
+
+        public void SetCommandMenu()
+        {
+        }
+
+        public void SetStartByte(int ID)
+        {
+            startByte_ = Section.StartByte + ID * 3;
+        }
+
+        public void SetLocalStartByte(CiStartId startID)
+        {
+            SetStartByte(ID - startID.SectionStart);
+        }
+
+        public void UpdateStatus(byte[] recvBuf, int nRecv)
+        {
+            const byte TRUE_VALUE = 0X05;
+            IsOccupied = (recvBuf[startByte_ + 1] >> 4) == TRUE_VALUE;
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            Rectangle rect = graphics_[0] as Rectangle;
+            rect.OnRender(dc, isOccupied_ ? Brushes.Red : Brushes.DarkGreen);
+            dc.DrawText(formattedName_, namePoint_);
+        }
+    }
+}
